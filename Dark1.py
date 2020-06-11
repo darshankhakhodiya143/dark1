2bea1d763dfba14a45d794322b57e908766e10be
darshankhakhodiya143 <66729710+darshankhakhodiya143@users.noreply.github.com>
2020-06-11 17:17:23 +0530

    Update Dark1.py

--- Dark1.py
+++ Dark1.py
@@ -476,8 +476,5 @@ def brute():
 
         except IOError:
             print '\033[1;97m[!] File not found...'
-            print """\n\033[1;97m[!] \033[1;97mLooks like you don't have a wordlist"""
-            super()
-
 if __name__ == '__main__':
 	login()
